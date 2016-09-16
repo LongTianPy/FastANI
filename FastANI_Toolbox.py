bda20b0f2406fb408cb8eedf007cdac1c5e87bb8
LongTianPy <tianlongapp@gmail.com>
2016-09-15 21:37:11 -0400

    [dev] object: class_tab function: parse_blast_tab in progress: FastANI

--- FastANI_Toolbox.py
+++ FastANI_Toolbox.py
@@ -9,6 +9,8 @@ from os import listdir
 from os.path import isfile, isdir, join
 import pandas as pd
 import numpy as np
+import multiprocessing as mp
+import argparse
 
 # OBJECT
 class blast_tab(object):
@@ -40,6 +42,15 @@ class blast_tab(object):
 
 
 # FUNCTION
+def get_parsed_args():
+    parser = argparse.ArgumentParser(
+        description="FastANI"
+    )
+    parser.add_argument("-i", dest="reference_folder", help="Folder where the FASTA files are")
+    parser.add_argument("-o", dest="work_dir", help="Output dir")
+    args = parser.parse_args()
+    return args
+
 def split_query_seq(query_seq, frag_size=1020):
     """
     input: object from concatenated query genome string
@@ -58,13 +69,18 @@ def split_query_seq(query_seq, frag_size=1020):
     return fragments
 
 
-def concate_reference_files(reference_folder, work_dir):
+def concate_reference_files(reference_folder):
+    """
+    Write reference files into one
+    :param reference_folder:
+    :return: A mapping file for multi-processing, [each_query_filepath, ref_prefix]
+    """
     reference = []
     append_reference = reference.append
-    reference_files = [file for file in listdir(reference_folder) if isfile(join(reference_folder, file))]
-    concat_ref_file = open(join(work_dir,'concat_ref.fasta'), 'w')
+    reference_files = [join(reference_folder,file) for file in listdir(reference_folder) if isfile(join(reference_folder, file))]
+    concat_ref_file = open('concat_ref.fasta', 'w')
     for file in reference_files:
-        ref_prefix = "".join(file.split(".")[:-1])
+        ref_prefix = "".join(file.split("/")[-1].split(".")[:-1])
         append_reference(ref_prefix)
         concat_ref_file.write(">{0}\n".format(ref_prefix))
         each_ref = open(file, "r")
@@ -76,18 +92,17 @@ def concate_reference_files(reference_folder, work_dir):
             seq = seq + seq_modified
         concat_ref_file.write(seq + "\n")
     concat_ref_file.close()
-    return ref_prefix
+    mapping = [[i, ref_prefix] for i in reference_files]
+    return mapping
 
 
-def makeblastdb(work_dir):
-    cmd = "makeblastdb -dbtype nucl -in {0}/concat_ref.fasta -title ref_genome " \
-          "-out {0}/ref_genome_blastdb".format(work_dir)
+def makeblastdb():
+    cmd = "makeblastdb -dbtype nucl -in concat_ref.fasta -title ref_genome " \
+          "-out {0}/ref_genome_blastdb"
     os.system(cmd)
 
 
-def parse_blast_tab(prefix_query, ref_prefix, work_dir):
-    FilePath_blast_tab = join(work_dir, "result.tab")
-    ANI_table = blast_tab(FilePath_blast_tab,ref_prefix).ANI_table
+def parse_blast_tab_get_best(ANI_table):
     top_hit = ANI_table.index[0]
     if top_hit != prefix_query:
         best_match = top_hit
@@ -95,33 +110,66 @@ def parse_blast_tab(prefix_query, ref_prefix, work_dir):
     else:
         best_match = ANI_table.index[1]
         best_ANI = ANI_table.loc[best_match,'ANI']
+    best_ANI = "{0:.5f}%".format(best_ANI*100)
     return best_match, best_ANI
 
-
-
-
-def FastANI(query_file_path, reference_folder, work_dir):
-    FilePath_NewGenome = query_file_path
-    prefix_query = "".join(query_file_path.split("/")[-1].split(".")[-1])
-    handler_NewGenome = open(FilePath_NewGenome, "r")
-    records_NewGenome = list(SeqIO.parse(handler_NewGenome, "fasta"))
+def single_blast_run(each_mapping):
+    """
+    Process single file against reference database
+    :param file: file path
+    :param work_dir:
+    :return:
+    """
+    (query_filepath,ref_prefix) = each_mapping
+    prefix_query = "".join(query_filepath.split("/")[-1].split(".")[:-1])
+    handler_NewGenome = open(query_filepath,"r")
+    records_NewGenome = list(SeqIO.parse(handler_NewGenome,"fasta"))
     handler_NewGenome.close()
     concatenated_seq_NewGenome = ""
     for record in records_NewGenome:
-        concatenated_seq_NewGenome = concatenated_seq_NewGenome + str(record.seq).replace("N", "")
-    fragments_NewGenome = split_query_seq(query_seq=concatenated_seq_NewGenome, frag_size=1020)
-    handler_fragments_NewGenome = open(join(work_dir, prefix_query, "query.fna"), "w")
+        concatenated_seq_NewGenome = concatenated_seq_NewGenome + str(record.seq).replace("N","")
+    fragments_NewGenome = split_query_seq(query_seq=concatenated_seq_NewGenome,frag_size=1020)
+    handler_fragments_NewGenome = open((prefix_query + "_query.fna"), "w")
     for i in range(len(fragments_NewGenome)):
         handler_fragments_NewGenome.write(">fragment_{0}\n".format(i))
-        handler_fragments_NewGenome.write(fragments_NewGenome[i] + "\n")
+        handler_fragments_NewGenome.write(fragments_NewGenome[i]+"\n")
     handler_fragments_NewGenome.close()
-    ref_prefix = concate_reference_files(reference_folder=reference_folder, work_dir=work_dir)
-    makeblastdb(work_dir=work_dir)
-    blastall_cmd = "blastall -p blastn -o {0}/result.tab -i {1} -d {2} " \
+    blastall_cmd = "blastall -p blastn -o {0}_result.tab -i {1} -d {2} " \
                    "-X 150 -q -1 -F F -e 1e-15 " \
-                   "-b 1 -v 1 -m 8 -a 4"\
-        .format(work_dir, join(work_dir,prefix_query,"_query.fna"), join(work_dir,"ref_genome_blastdb"))
+                   "-b 1 -v 1 -m 8 -a 4" \
+        .format(prefix_query, prefix_query + "_query.fna", "ref_genome_blastdb")
     os.system(blastall_cmd)
-    best_match, best_ANI = parse_blast_tab(prefix_query=prefix_query, ref_prefix=ref_prefix, work_dir=work_dir)
+    FilePath_blast_tab = prefix_query + "_result.tab"
+    ANI_table = blast_tab(FilePath_blast_tab, ref_prefix).ANI_table
+    best_match, best_ANI = parse_blast_tab_get_best(ANI_table=ANI_table)
+    ANI_table.columns = [prefix_query]
+    with open("best_match.tab","a") as best_table:
+        best_table.write("{0}\t{1}\t{2}\n".format(prefix_query, best_match, best_ANI))
+    ANI_table.to_csv("{0}_ANI.csv".format(prefix_query))
 
+
+def FastANI(argv=None):
+    if argv is None:
+        argv = sys.argv
+    args = get_parsed_args()
+    reference_folder = args.reference_folder
+    work_dir = args.work_dir
+    os.chdir(work_dir)
+    # Prepare blast db
+    mapping = concate_reference_files(reference_folder=reference_folder)
+    makeblastdb()
+    # Multi-processing
+    pool = mp.Pool(processes=4)
+    pool.map(single_blast_run, mapping)
+    ref_prefix = mapping[0][1]
+    df = pd.DataFrame(index=ref_prefix)
+    pairwise_ANI_files = [file for file in listdir("./") if file.endswith(".csv")]
+    for file in pairwise_ANI_files:
+        each_df = pd.DataFrame.from_csv(file, header=0, index_col=0)
+        colname = each_df.columns[0]
+        df[colname] = each_df
+    df.to_csv("pairwise_ANI.csv")
+
+if __name__ == "__main__":
+    FastANI()
 
